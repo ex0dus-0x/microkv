a44f56b142896e1caf7dcc24cd1f7ecaa28c2ee2
ex0dus-0x <17283637+ex0dus-0x@users.noreply.github.com>
2018-04-08 20:33:52 -0400

    Added KV support

--- src/lib.rs
+++ src/lib.rs
@@ -9,13 +9,17 @@ use serde_json::{Map, Value};
 use std::fs;
 use std::path::PathBuf;
 
-enum StoreError {
+// Rather than using a HashMap, a Map is much more optimized for JSON interactions
+type KeyValue = Map<String, Value>;
+
+pub enum StoreError {
     TinyStoreNew(String),
 }
 
 pub struct TinyStore {
-    path: Option<PathBuf>,           // this is the path where the database file will be written to, if the user chooses to commit
-    hash: Option<String>,           // implement a hash algorithm for values that store sensitive data
+    path: Option<PathBuf>,          // this is the path where the database file will be written to, if the user chooses to commit
+    hash: Option<String>,           // implement a hash algorithm for values that store sensitive data, if the user chooses to
+    storage: KeyValue,
 }
 
 impl Default for TinyStore {
@@ -26,56 +30,64 @@ impl Default for TinyStore {
         TinyStore {
             path: Some(PathBuf::from("./tmp/database.json")),
             hash: None,
+            storage: KeyValue::new(), 
         }
     }
 }
 
 impl TinyStore {
+    
 
+    // Creates a new TinyStore object without any configuration. 
+    // Assumes user is utilizing now hashing algorithm and wants to persist data in a file.
     pub fn quick_new() -> TinyStore {
         // Creates a new database utilizing default struct values
         TinyStore::default()
     }
-
+    
+    // Creates a new TinyStore object with configuration supplied by parameters
     pub fn new(path: Option<String>, hash_algo: Option<String>) -> TinyStore {
         
         // Check if path was supplied 
         if let None = path {
-            
             // Create new TinyStore with no path
             TinyStore {
                 path: None,
                 hash: hash_algo,
+                storage: KeyValue::new(),
             }
         } else {
-
             // Create new TinyStore with path
             TinyStore {
                 path: Some(PathBuf::from(path.unwrap())),
                 hash: hash_algo,
+                storage: KeyValue::new(),
             }
         }
     }
     
+    pub fn write<T: Serialize>(&self, key: String, value: &T) -> Result<(), StoreError> {
+        let kv = KeyValue::new();
+    
+        
+        Ok(())
+    }   
+    
     /*
-    pub fn write<T: Serialize>(self, id: &T) -> Result<(), StoreError> {
-
-    }
-
     pub fn get(self, id: String) -> Result<(), StoreError> {
 
     }
 
-    pub fn get_all(self) -> Result<String, StoreError> {
+    pub fn get_all(self) -> Result<Vec<Container>, StoreError> {
 
     }
     
     pub fn delete(self, id: String) -> Result<(), StoreError> {
 
     }
-    
-    pub fn commit(self) -> Result<(), StoreError> {
 
+    pub fn commit(self) -> Result<(), StoreError> {
+        // check to see if PathBuf is path to a file, not directory
 
     }
 
