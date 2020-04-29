42594edbf38958fb7f87f36362a12a07ce3141b3
ex0dus-0x <17283637+ex0dus-0x@users.noreply.github.com>
2020-04-29 14:58:49 -0400

    Initialize unit tests

---
+++ tests/tests.rs
@@ -0,0 +1,17 @@
+//! tests.rs
+//!
+//!     MicroKV module unit testing suite.
+//!
+//!     Defines unit tests for:
+//!         - simple database interactions
+//!         - concurrent database interactions
+
+extern crate microkv;
+
+use microkv::MicroKV;
+
+
+#[test]
+fn test_get() {
+    assert_eq!(1, 1);
+}
