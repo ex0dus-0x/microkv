293a87911cb1425a9a14a62d454ffb9cad02c864
ex0dus-0x <17283637+ex0dus-0x@users.noreply.github.com>
2020-05-06 16:25:59 -0400

    Added more database helper calls

--- src/ser.rs
+++ src/ser.rs
@@ -6,29 +6,28 @@
 //!     Inspired by:
 //!     https://users.rust-lang.org/t/how-to-serialize-deserialize-an-async-std-rwlock-t-where-t-serialize-deserialize/37407/2
 
-
 // WIP!!
 #[allow(dead_code)]
 pub mod arclock {
 
     use std::sync::{Arc, RwLock};
 
-    use serde::{Serialize, Deserialize};
     use serde::de::Deserializer;
     use serde::ser::Serializer;
-
+    use serde::{Deserialize, Serialize};
 
     pub fn serialize<S, T>(val: &Arc<RwLock<T>>, s: S) -> Result<S::Ok, S::Error>
-        where S: Serializer,
-              T: Serialize,
+    where
+        S: Serializer,
+        T: Serialize,
     {
         T::serialize(&*val.read().unwrap(), s)
     }
 
-
     pub fn deserialize<'de, D, T>(d: D) -> Result<Arc<RwLock<T>>, D::Error>
-        where D: Deserializer<'de>,
-              T: Deserialize<'de>,
+    where
+        D: Deserializer<'de>,
+        T: Deserialize<'de>,
     {
         Ok(Arc::new(RwLock::new(T::deserialize(d)?)))
     }
